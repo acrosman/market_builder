42cdec54155bb7512ea09dd15268c77480cae4ec
Aaron Crosman <acrosman@gmail.com>
2025-12-31 16:17:49 -0500

    Removing ideas about features that aren't getting hard coded.

--- src/farm.js
+++
@@ -1,9 +0,0 @@
-const { producer } = require('./producer.js');
-
-class farm extends producer {
-  constructor(name, employees, goodsList, xCoordinate, yCoordinate) {
-    super('farm', name, employees, goodsList, xCoordinate, yCoordinate);
-  }
-}
-
-module.exports = farm;
